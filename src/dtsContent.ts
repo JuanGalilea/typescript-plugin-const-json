18f2b1739586bd73601abe5fadf392b80928a7a5
JuanGalilea <juangk93@gmail.com>
2025-10-24 10:52:55 +0200

    create declaration files for files ending in schema.json

--- src/dtsContent.ts
+++ src/dtsContent.ts
@@ -1,5 +1,5 @@
 import { Logger } from './logger'
-import { readFileSync } from 'fs'
+import { readFileSync, writeFileSync } from 'fs'
 
 export const getDtsContent = (fileName: string, logger: Logger): string => {
   const text = readFileSync(fileName, 'utf-8')
@@ -10,6 +10,9 @@ export const getDtsContent = (fileName: string, logger: Logger): string => {
 declare const data = ${content}${' as const'}
 export default data
 `
+    if (fileName.toLowerCase().endsWith('schema.json')) {
+      writeFileSync(fileName.replace('.json', '.d.json.ts'), dts, 'utf-8')
+    }
   } catch (e: unknown) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     logger.error(e as any)
